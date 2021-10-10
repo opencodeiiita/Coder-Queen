1b7373ed77a4d6c19230089a5f88d9b3174a37b8
medhatiwari <medhavns1@gmail.com>
2021-10-10 10:21:07 +0530

    #23 Added the code for drawer icon

--- flutternews365/lib/home.dart
+++ flutternews365/lib/home.dart
@@ -107,6 +107,125 @@ class _HomeState extends State<Home> {
               title: Text("Profile")),
         ],
       ),
+
+      //Drawer
+      drawer: Container(
+        width: MediaQuery.of(context).size.width / 1.25,
+        child: Drawer(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 30,
+              ),
+              DrawerHeader(
+                child: Container(
+                    height: 142,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset(
+                      "assets/news.png",
+                    )),
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    currentIndex = 3;
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontFamily: 'Avenir',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  'Settings',
+                  style: TextStyle(
+                    fontFamily: 'Avenir',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              Text(
+                'About',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              Text(
+                'Log Out',
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 45,
+              ),
+              Material(
+                borderRadius: BorderRadius.circular(500),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(500),
+                  splashColor: Colors.black45,
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.black,
+                    child: Icon(Icons.arrow_back, color: Colors.yellow),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 65,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.black,
+                  child: Center(
+                    child: Text(
+                      'v1.0.1',
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 20,
+                        color: const Color(0xffffffff),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
